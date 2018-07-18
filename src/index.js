0472c3da2aa3770db562d41c43f18264bf764dd8
jayrgee <jayrgee@fastmail.fm>
2018-07-18 22:23:51 +1000

    rename vfDemo

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import 'babel-polyfill';
 
-import blah from './blah';
+import vfDemo from './vfDemo';
 import sayHello from './hello';
 import './index.scss';
 
 document.getElementById('root').innerHTML = sayHello();
 
 const vex = document.getElementById('vex');
-blah(vex.id);
+vfDemo(vex.id);
